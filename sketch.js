3249a39e059244bac36e18e4ee9a12a57fcf4a93
Eliott Ham <eliotthham@gmail.com>
2016-12-13 20:18:17 -0800

    Created base 16 x 16 grid with 'X' as each grid square.

---
+++ sketch.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	var row = 16;
+	var col = 16;
+
+	for(var i = 1; i <= row; i++){
+		$("#container").append('<div id="row' + i + '" class="rows"></div>');
+		for(var j = 1; j <= col; j++){
+			$('#row' + i + '').append('<div id="col' + j + '" class="cols">X</div>');
+		}
+	}
+		
+	});
