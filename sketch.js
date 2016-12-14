bb4bf0af83d830bdd607d0fe5f8f8dc5b3a89931
Eliott Ham <eliotthham@gmail.com>
2016-12-13 20:51:12 -0800

    Created initial 16 x 16 grid with black squares. jQuery used to turn squares blue if hovered over.

--- sketch.js
+++ sketch.js
@@ -5,8 +5,11 @@ $(document).ready(function(){
 	for(var i = 1; i <= row; i++){
 		$("#container").append('<div id="row' + i + '" class="rows"></div>');
 		for(var j = 1; j <= col; j++){
-			$('#row' + i + '').append('<div id="col' + j + '" class="cols">X</div>');
+			$('#row' + i + '').append('<div id="col' + j + '" class="cols"></div>');
 		}
 	}
+	$(".cols").hover(function(){
+		$(this).css("background-color", "blue");
+	});
 		
 	});
